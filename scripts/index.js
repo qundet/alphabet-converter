dc34cf05f5eaf3e62aadb9bb297c7225d6280f82
KUNDET <kundetovm@gmail.com>
2024-01-07 18:55:26 +0300

    version 1

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,21 @@ function switchAlphabet() {
   to.innerHTML = tmp;
   console.log(alph);
 }
+
+function onInput() {
+  setTimeout(() => {
+    let val = document.getElementById("input").value;
+    let translated = cyrillicToLatin(val);
+    document.getElementById("output").value = translated;
+  }, 1000);
+}
+
+function cyrillicToLatin(str) {
+  for (let i = 0; i < cyrillicAlph.length; i++) {
+    const regex = new RegExp(`${cyrillicAlph[i]}`, "g");
+    str = str.replace(regex, latinAlph[i]);
+    console.log(str);
+  }
+  console.log(str);
+  return str;
+}
